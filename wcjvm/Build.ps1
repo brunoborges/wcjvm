abf2faf72dd27aef34867548622fe4878579fcea
Bruno Borges <brborges@microsoft.com>
2023-09-26 23:59:51 -0700

    Oops

--- wcjvm/Build.ps1
+++ wcjvm/Build.ps1
@@ -5,10 +5,10 @@ dotnet publish -c Release -o out
 docker build -t wcjvm -f Slim.Dockerfile .
 
 # Run the Docker container in Process Isolation Mode with limited memory/cpu
-docker run --memory=256m --cpus 2 --isolation=process -ti wcjvm
+docker run --memory=256m --cpus 2 --isolation=process wcjvm
 
 docker build -t javaapp -f .\JavaApp.Dockerfile .
 
 # Run a Java application in Process Isolation Mode with limited memory/cpu
-docker run --memory=512m --cpus 2 --isolation=process -ti javaapp
+docker run --memory=512m --cpus 2 --isolation=process javaapp
 
